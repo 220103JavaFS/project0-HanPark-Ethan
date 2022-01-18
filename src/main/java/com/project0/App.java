ae0cf3f0890942581e73808e0525a6cb6ef2ac49
HanPark-Ethan <ethan.hanp89@gmail.com>
2022-01-17 23:44:37 -0500

    updated App.java file, added new controllers to configure

--- src/main/java/com/project0/App.java
+++ src/main/java/com/project0/App.java
@@ -1,7 +1,9 @@
 package com.project0;
 
 import com.project0.controllers.Controller;
+import com.project0.controllers.DepartmentController;
 import com.project0.controllers.LoginController;
+import com.project0.controllers.RoleController;
 import io.javalin.Javalin;
 
 public class App {
@@ -10,7 +12,7 @@ public class App {
     public static void main(String[] args){
 
         app = Javalin.create();
-        configure(new LoginController());
+        configure(new RoleController(), new DepartmentController(), new LoginController());
         app.start();
 
     }
