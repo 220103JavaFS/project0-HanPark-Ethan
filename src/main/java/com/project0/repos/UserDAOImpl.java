d2fc6cd96025dfdcd84a77a12bc81da9343bd303
HanPark-Ethan <ethan.hanp89@gmail.com>
2022-01-18 10:02:22 -0500

    added addUser(). Testing still required for output.

--- src/main/java/com/project0/repos/UserDAOImpl.java
+++ src/main/java/com/project0/repos/UserDAOImpl.java
@@ -5,10 +5,7 @@ import com.project0.models.Role;
 import com.project0.models.User;
 import com.project0.utils.ConnectionUtil;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,6 +28,13 @@ public class UserDAOImpl implements UserDAO {
                 user.setPhoneNumber(result.getString("user_phone"));
                 user.setDob(result.getString("user_dob"));
                 user.setSalary(result.getDouble("salary"));
+                String roleName = result.getString("role_name");
+                if (roleName != null){
+                    Role role = roleDAO.findByRole(roleName);
+                    user.setRole(role);
+                }
+                Department deptName = new Department(result.getString("dept_name"), result.getString("dept_descr"));
+                user.setDepartment(deptName);
                 list.add(user);
             }
             return list;
@@ -43,7 +47,7 @@ public class UserDAOImpl implements UserDAO {
     @Override
     public User findUser(int id) {
         try(Connection conn = ConnectionUtil.getConnection()){
-            String sql = "SELECT * FROM (SELECT * FROM users LEFT JOIN roles ON roles.role_name = users.role_name AS employee_name WHERE user_id = 1 "+id+";";
+            String sql = "SELECT * FROM (SELECT * FROM users LEFT JOIN roles ON roles.role_name = users.role_name AS employee_name WHERE user_id = "+id+";";
             Statement statement = conn.createStatement();
             ResultSet result = statement.executeQuery(sql);
             User user = new User();
@@ -73,6 +77,26 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public boolean addUser(User user) {
+        try (Connection conn = ConnectionUtil.getConnection()){
+            String sql = "UPDATE users SET first_name = ?, last_name = ?, user_email = ?, user_phone = ?, user_dob = ?, dept_names = ?, role_names = ? WHERE role_name = ?;";
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            int count = 0;
+            statement.setString(++count, user.getFirstName());
+            statement.setString(++count, user.getLastName());
+            statement.setString(++count, user.getEmailAddress());
+            statement.setString(++count, user.getPhoneNumber());
+            statement.setString(++count, user.getDob());
+            statement.setString(++count, user.getRole().getRoleName());
+            statement.setString(++count, user.getDepartment().getDeptName());
+            statement.setDouble(++count, user.getSalary());
+            statement.execute();
+
+            return true;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return false;
     }
 
